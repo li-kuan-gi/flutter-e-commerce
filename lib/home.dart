20c88d877d65ae321e21749f8670f40d11bf9460
ie31415926 <ie31415926@gmail.com>
2025-01-06 01:54:23 +0000

    basic home page

--- lib/home.dart
+++ lib/home.dart
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shrine/model/product.dart';
+import 'package:shrine/model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -23,13 +26,91 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
-      // TODO: Add app bar (102)
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+        ),
+        title: const Text('SHRINE'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.tune),
+          ),
+        ],
+      ),
       // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(context),
       ),
       resizeToAvoidBottomInset: false,
     );
+  }
+
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights
+        child: Column(
+          // TODO: Center items on the cards
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                // TODO: Align labels to the bottom and center
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // TODO: Change inner-most Column
+                children: [
+                  // TODO: Handle overflowing labels
+                  Text(
+                    product.name,
+                    style: theme.textTheme.titleLarge,
+                    maxLines: 1,
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  Text(
+                    formatter.format(product.price),
+                    style: theme.textTheme.titleSmall,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }).toList();
   }
 }
